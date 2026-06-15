ee9c46edeb19ffd4b2728708a1f6d257d36a3596
24h41a6113-maha <24h41a6113@bvcits.edu.in>
2026-06-15 18:25:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ client = Groq(api_key=st.secrets["GROQ_API_KEY"])
 
 def ask_groq(prompt):
     response = client.chat.completions.create(
-        model="llama3.3-70b-versatile",
+        model="llama-3.3-70b-versatile",
         messages=[{"role": "user", "content": prompt}],
         max_tokens=500
     )
